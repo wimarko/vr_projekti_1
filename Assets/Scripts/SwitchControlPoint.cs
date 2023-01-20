79204eb15328d0d24cfe8fdb1fedc43f33e3c565
wimarko <marko.wiik@metropolia.fi>
2023-01-20 12:58:47 +0200

    Button Jostain tutoriaalista

--- Assets/Scripts/SwitchControlPoint.cs
+++ Assets/Scripts/SwitchControlPoint.cs
@@ -7,20 +7,60 @@ public class SwitchControlPoint : MonoBehaviour
 {
     public InputActionReference switchReference = null;
     public GameObject hiddenPlatform = null;
-    // Start is called before the first frame update
+
+    [SerializeField] GameObject buttonSphere;
+
+    private float buttonDownDistance = 0.025f;
+    private float buttonReturnSpeed = 0.001f;
+    private float buttonOriginalX;
+
+    private float hitAgainTime = 0.5f;
+    private float canHitAgain;
+    private bool buttonHit = false;
+    private bool active = false;
+
     void Start()
     {
-        
+        buttonOriginalX = buttonSphere.transform.position.x;
     }
 
-    // Update is called once per frame
+    
     void Update()
     {
+        if (buttonHit == true)
+        {
+            buttonHit = false;
+
+            active = !active;
+
+            buttonSphere.transform.position =
+            new Vector3(
+            buttonSphere.transform.position.x - buttonDownDistance,
+            buttonSphere.transform.position.y,
+            buttonSphere.transform.position.z);
+
+
+            //hidden platform gets same active status as button
+            //true/false  (jos toimii)
+            hiddenPlatform.SetActive(active);
+        }
+
+        
+
+        if(buttonSphere.transform.position.x > buttonOriginalX) {
+            buttonSphere.transform.position += new Vector3(buttonReturnSpeed, 0, 0);
+        }
         
     }
 
-    public void SwitchTheSwitch()
+    private void OnTriggerStay(Collider other)
     {
-        hiddenPlatform.SetActive(!gameObject.activeInHierarchy);
+        if(other.CompareTag("PlayerHand") && canHitAgain < Time.time)
+        {
+            canHitAgain = Time.time + hitAgainTime;
+            buttonHit= true;
+        }
     }
+
+    
 }
